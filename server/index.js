b3a0ea1a8067c88ea372d463347e4060832b2415
Purujit Vij <130056678+purujitv@users.noreply.github.com>
2025-02-28 17:14:18 +0530

    Connection string changed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/chatbot', {
+mongoose.connect('mongodb+srv://purujitzinreet:humvaFlTXcCRGI1i@cluster0.tqaj1.mongodb.net/chatbot', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected'))
